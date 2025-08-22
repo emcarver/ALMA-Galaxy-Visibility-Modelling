e20011a0018222136396768ed6ce79444086d89f
emcarver <e.carver@uleth.ca>
2025-08-22 13:23:24 -0700

    Update run_fittings.py with more comments

--- radial_version/run_fittings.py
+++ radial_version/run_fittings.py
@@ -1,5 +1,5 @@
 #Some things are commented out here, as they seemed helpful at some points, but either are no longer necessary, or cause problems.
-#If you have a proper TeX installation: fix lines 28, 140, 143. 
+#If you have a proper TeX installation: fix lines 28, 188, 191. 
 ###%%%###%%%###%%%###%%%###%%%###
 
 ###%%%###%%%### Package Imports ###%%%###%%%###
@@ -14,7 +14,7 @@ import pandas as pd
 from scipy.special import j1
 from astropy import units as u
 from multiprocessing import Pool
-from astropy.units import Quantity
+from astropy.units import Quantity #Note: when I went through adding more comments to the functions, I realized this might be redundant and left over from previous troubleshooting. Unable to remove and test to check that though..
 from emcee import EnsembleSampler as es
 from astropy.coordinates import SkyCoord
 from galario.double import get_image_size, chi2Profile
@@ -39,25 +39,56 @@ os.environ["OMP_NUM_THREADS"] = "1"
 
 ###%%%###%%%### Fitting profiles ###%%%###%%%###  
 def radial_gaussian_ring(peak, sigma, ring_rad, start, step, numsteps):
+    """
+    Make a 1d profile of a ring with a gaussian shape, in the formatting required for use in sampleProfile and chi2Profile.
+    peak, sigma: peak and width of the gaussian
+    ring_rad: radial position of the gaussian centre
+    start, step, numsteps: origin, grid spacing, grid size of the radial grid to evaluate the function on
+    """
     radius = np.linspace(start, start + numsteps*step, numsteps)
     return peak*np.exp((-1/2)*((radius-ring_rad)/sigma)**2)
 
 def radial_jinc(normalization, width, offset, start, step, numsteps):
+    """
+    Make a 1d profile of a jinc function, in the formatting required for use in sampleProfile and chi2Profile.
+    normalization, width: peak and width of the jinc function
+    offset: a 'vertical' offset that can be used to keep the brightness function nonzero
+    start, step, numsteps: origin (if =0 will can cause divide by zero errors), grid spacing, grid size of the radial grid to evaluate the function on
+    """
     radius = np.linspace(start, start + numsteps*step, numsteps)
     return normalization*j1(width*radius)/radius + offset
 ###%%%###%%%###%%%###%%%###%%%###%%%###%%%###%%%###
 
 ###%%%###%%%### Emcee evaluation functions ###%%%###%%%###
 def lnpriorfn(p, par_ranges):
+    """
+    Check if the parameters are within the defined range (check if meeting prior condition)
+    p: list containing the parameter vector
+    par_ranges: array containing parameter ranges
+    This is a function only ever accessed by lnpostfn
+    """
     for i in range(len(p)):
         if isinstance(par_ranges[i][0], str) or isinstance(par_ranges[i][1], str):
-            logging.error("Bad param_ranges at index {i}: {par_ranges[i]}")
+            logging.error(f"Bad param_ranges at index {i}: {par_ranges[i]}")
             raise ValueError("Non-numeric bounds detected in param_ranges")
         if p[i] < par_ranges[i][0] or p[i] > par_ranges[i][1]:
             return -np.inf
     return 0.0
 
 def lnpostfn(params, param_ranges, fitType, start, step, numsteps, nxy, dxy, u, v, re, im, w):
+    """
+    Implement the posterior function. Some unit conversions are performed in order to keep galario happy 
+    and also make the user-facing setup in nice thinkable units. 
+
+    params: list containing the parameter vector
+    param_ranges: array containing parameter ranges
+    fitType: a string indicating which fit profile to use
+    start, step, numsteps: origin, grid spacing, grid size of the radial grid to evaluate the function on (float)
+    nxy: extent of fitted image in pixels. Powers of two preferred for speed (int)
+    dxy: the pixel size of the image in radians (float)
+    u, v: arrays of the u and v coordinates of the observed visibilities in units of wavelengths
+    re, im, w: Re(V), Im(V), and weight of the observed visibilities
+    """
     lnprior = lnpriorfn(params, param_ranges)  # apply prior
     if not np.isfinite(lnprior):
         return -np.inf
@@ -89,11 +120,15 @@ def lnpostfn(params, param_ranges, fitType, start, step, numsteps, nxy, dxy, u, 
 
     chi2 = chi2Profile(f, start, step, nxy, dxy, u, v, re, im, w,
                        inc=inclination, PA=posangle, dRA=dRA, dDec=dDec)
-    return -0.5 * chi2 #+ lnprior
+    return -0.5 * chi2 #+ lnprior  #is zero if the code gets to this stage. If not using a uniform prior, take this into account here?
 ###%%%###%%%###%%%###%%%###%%%###%%%###%%%###%%%###%%%###%%%###%%%###
 
 ###%%%###%%%### Functions to do all the work ###%%%###%%%###
 def read_param_file(file):
+    """
+    Read in parameters from the provided parameter file, with capabilities to read strings, floats, and ints. 
+    file: string containing file path to a parameter file
+    """
     params={}
     with open(file, 'r') as f:
         for line in f:
@@ -108,6 +143,11 @@ def read_param_file(file):
     return params
 
 def initialize_data(datatable, frequency):
+    """
+    Just a wrapper on galario get_image_size and the unpacking of my uvtable since I did those two together repeatedly. 
+    datatable: string containing the filepath to a text file holding the observed visibility data in a uvtable (use uvplot package)
+    frequency: observed frequency (float)
+    """
     u, v, re, im, w = np.require(np.loadtxt(datatable, unpack=True), requirements='C')
     wavelength = 299792458/frequency
     u /= wavelength
@@ -117,6 +157,14 @@ def initialize_data(datatable, frequency):
     return  nx, dx, u, v, re, im, w
 
 def save_results(fittype, sampler, pos, outname): #prob, state
+    """
+    Save the outputs of the sampler for further analysis (given the fact that this is run as a script in a headless session) if necessary.
+    Also creates the corner plot
+    fittype: string, indicating which fit profile to use
+    sampler: an ensembleSampler object that the fiting was run on (want to access the chain)
+    pos: array, final position vector of all the walkers. Used later to find the fitted parameter values
+    outname: string, indicating the base filename to save products to
+    """
     np.savetxt(outname+'_pos.txt', pos, fmt='%10.6e', delimiter='\t')
     #I previously had been saving ALL the outputs, as I thought I would use them all. I did not. Uncomment and add prob, state to the args if you want to save these 
     #np.savetxt(outname+'_prob.txt', prob, fmt='%10.6e', delimiter='\t')
@@ -157,6 +205,14 @@ def save_results(fittype, sampler, pos, outname): #prob, state
         logging.error('Something went wrong in the creation of the corner plot. Moving on...', exc_info=True)
 
 def main(paramfile, fitType, ranges, guesses, productname):
+    """
+    Main function that takes care of the entire fitting process
+    paramfile: string, path to the gloval parameter file
+    fitType: string, indicating which fitting profile to use
+    ranges: string, path to a csv file containing the allowed parameter ranges
+    guesses: string, path to a csv file containing the initial parameter guesses
+    productname: string, indicating what base filename to use when generating various products. 
+    """
     #Set up the logger
     logging.basicConfig(filename=productname+'.log', filemode='w', level=logging.INFO)
 
